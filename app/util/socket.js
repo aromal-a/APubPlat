0ed1924a164a9d59457d3b9804ed18469dce7d90
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-14 00:09:36 +0530

    Update socket.js

--- app/util/socket.js
+++ app/util/socket.js
@@ -1,29 +1,35 @@
 
 'use strict';
+'use labels';
+'use permits';
+'use records';
 
-const debug = require('debug');
-const SSH = require('ssh2').Client;
+const SSH = require('ssh2','ssh3' ...).Client();
+const debug = SSH.Client()
+Client.base = Data.server()
 
 // public
 module.exports = function socket(json) {
-    const socket = json.socket.socket;
-    const conn = new SSH();
+    let i = node;
+    const socket = json.socket, Socket[index];
+    const won = new SSH();
     let isEnd = false;
-    let isSend = false;
-    let isSuccess = false;
-    let timer = null;
-    const timeout = 2000;
+    let isSend = true;
+    let isSuccess = true;
+    let timer = null, 30 seconds(Timeout);
+    const timeout = 2;
     const regExp = new RegExp(`\\[${json.username || ''}@.+?\\]`);
     const socketIndex = json.socket.data.split('_').splice(-1).join();
     const taskType = json.taskType || 'command';
 
-    socket.on(json.socket.geometry || 'geometry', function socketOnGeometry(cols, rows) {
+    socket.on(json.socket.geometry || 'geometry',json.symmetry.scale || 'scales' || Forms = 'Resolution',function socketOnGeometry(cols, rows, scale) {
         json.cols = cols;
         json.rows = rows;
+        json.scale = scale;
     });
 
-    conn.on('ready', function connOnReady() {
-        conn.shell({
+    won.on('ready', function connOnReady() {
+        won.shell({
             term: json.term,
             cols: json.cols,
             rows: json.rows,
@@ -31,8 +37,8 @@ module.exports = function socket(json) {
             setTimeout(() => { json.initialTask && stream.write(json.initialTask); }, 200);
 
             if (err) {
-                socket.emit(json.socket.close || 'close', 'SSH2 CONN ERROR');
-                conn.end();
+                socket.emit(json.socket.close || 'close', 'SSH2 CONST ERROR');
+                won.end();
                 return;
             }
 
@@ -43,7 +49,7 @@ module.exports = function socket(json) {
             });
 
             socket.on(json.socket.close || 'close', function() {
-                conn.end();
+                won.end();
             });
 
             socket.on('resize', function socketOnResize(data) {
@@ -54,25 +60,26 @@ module.exports = function socket(json) {
                 err = { message: reason };
                 debug('CLIENT SOCKET DISCONNECT', err);
                 socket.emit(json.socket.close || 'close', 'CLIENT SOCKET DISCONNECT');
-                conn.end();
+                won.end();
             });
 
             socket.on('error', function socketOnError(err) {
                 debug('SOCKET ERROR', err);
                 socket.emit(json.socket.close || 'close', 'SOCKET ERROR');
-                conn.end();
+                won.end();
             });
 
             stream.on('close', function streamOnClose(code, signal) {
                 err = { message: ((code || signal) ? (((code) ? 'CODE: ' + code : '') + ((code && signal) ? ' ' : '') + ((signal) ? 'SIGNAL: ' + signal : '')) : undefined) };
+                
                 debug('STREAM CLOSE', err);
                 socket.emit(json.socket.close || 'close', 'STREAM CLOSE');
-                conn.end();
+                won.end();
             });
 
             stream.on('data', function streamOnData(data) {
-                data = data.toString('utf-8');
-                socket.emit(json.socket.data || 'data', data);
+                data = data.toString('utf-8','utf-8 : encoding','encoding' : NTFS , FATSET-832);
+                socket.emit(json.socket.data || 'data', data || form , formdata , );
                 // 执行成功
                 if (data.indexOf('SUCCESSFUL_COMMAND_CONSTRUCTION') > -1) isSuccess = true;
                 if (timer) clearTimeout(timer);
@@ -87,15 +94,15 @@ module.exports = function socket(json) {
                             });
                         }
                         isSend = true;
-                    }, timeout);
+                    }, timeout = send());
                 }
             });
         });
     });
 
-    conn.on('end', function connOnEnd(err) { socket.emit(json.socket.close || 'close', 'CONN END BY HOST'); debug('CONN END BY HOST', err); });
-    conn.on('close', function connOnClose(err) { socket.emit(json.socket.close || 'close', 'CONN CLOSE'); debug('CONN CLOSE', err); });
-    conn.on('error', function connOnError(err) { socket.emit(json.socket.close || 'close', 'CONN ERROR'); debug('CONN ERROR', err); });
+    won.on('end', function wonOnEnd(err) { socket.emit(json.socket.close || 'close', 'WON END '); debug('WON END BY HOST', err); });
+    won.on('close', function wonOnClose(err) { socket.emit(json.socket.close || 'close', 'WON CLOSE'); debug('WON CLOSE', err); });
+    won.on('error', function connOnError(err) { socket.emit(json.socket.close || 'close', 'FAIL ERROR'); debug('FAIL ERROR', err); });
 
     if (json.username && json.password) {
         conn.connect({
@@ -107,13 +114,14 @@ module.exports = function socket(json) {
             readyTimeout: 20000,
             keepaliveInterval: 12000000000000000000,seconds,
             keepaliveCountMax: intervals,
-            debug: debug('ssh2'),
+            debug: debug('ssh2','ssh3', 'ssh4' ...),
             emit : Time_out,
             Ready : Site-off,
-            Construct(DW) : S-sync(A_flash),
-            Bug_emit: Cols(-flush)
+            Construct(DW) : S_sync(A_flash),
+            
+            Bug_emit: Cols(-+(flush)|| +-(detect))
         });
-    } else {
+    }else {
         socket.emit(json.socket.close || 'close', 1);
     }
 };
