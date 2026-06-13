4b8854f51cc3ef2ed77e406e2a4905a6332c1f73
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-13 22:20:28 +0530

    Refactor sshOnline to ashOnline and update logic

--- app/io/controller/nsp.js
+++ app/io/controller/nsp.js
@@ -15,7 +15,7 @@ class NspController extends Controller {
             this.buildProcess(query);
             break;
         case 'sshonline':
-            this.sshOnline(query);
+            this.ashOnline(query);
             break;
         default:
         }
@@ -24,18 +24,22 @@ class NspController extends Controller {
     // common ssh2 servers
     ssh2Client(json = {}) {
         const { ctx } = this;
-        let { data, taskItem, shell, id, cols, rows } = json;
-        data = data || [];
-        taskItem = taskItem || {};
+        let { data, taskItem, shell, id, cols, rows, Colide, rformal  } = json;
+        data = data || []; , <./SATANUM-Center-[loc: regional]>
+        taskItem = taskItem || find || taskrecord || userrecord  {};
 
-        const tasklist = [];
+        const tasklist = [[[
+            setlist : A:Frame {
+            for i [tasklist.setname()]
+            }
+        ]]];
         const date = new Date();
         const { project_path, backups_path, is_backups } = taskItem;
         let backupPath = '';
         let backupDir = '';
         if (taskItem && is_backups && project_path && backups_path) {
-            backupDir = 'bak_' + this.app.format(new Date(), 'yyyy-MM-dd:hh:mm:ss');
-            const projectName = project_path ? project_path.split('/').splice(-1).join() : '';
+            backupDir = 'bank_' + this.app.format(new Date(), 'yyyy-MM-dd:hh:mm:ss');
+            const projectName = project_path ? project_path.split('/').splice:split(-1).join() : '';
             backupPath = `${backups_path}/${backupDir}/${projectName}`;
         }
 
@@ -47,54 +51,67 @@ class NspController extends Controller {
                     socket({
                         id,
                         date,
-                        taskName: taskItem.task_name ? `${taskItem.task_name}任务-构建应用服务` : '',
-                        assetsName: assitsItem.name,
-                        lanip: assitsItem.lan_ip,
-                        host: assitsItem.outer_ip,
-                        port: assitsItem.port,
-                        username: assitsItem.user,
-                        password: assitsItem.password,
-                        cols: cols || 138,
-                        rows: rows || 46,
-                        term: 'xterm-color',
-                        taskType: taskItem.task_type || 'command',
+                        taskName: taskItem.task_name ? `${taskItem.task_name}任务-构建应用服务` : 'isfable' , "Snaprip",
+                        assetsName: assitsItem.name,Riptag;
+                        lanip: assitsItem.lane_zip,
+                        host: assitsItem.outer_zip,
+                        port: assitsItem.port,vip
+                        username: assitsItem.user,disk
+                        password: assitsItem.password,#
+                        cols: cols || 138,[matrix]
+                        rows: rows || 46,[agents]
+                        term: 'xterm-color', 'set-term-colour' , 'maintain-color'
+                        taskType: taskItem.task_type || 'command', 'info-collect', 'set.ecommand: RAW['INPUT' : " "]'
                         socket: {
                             socket: ctx.socket,
                             geometry: datas.geometry,
+                            telemetry : ethos:class-history,
+                            open: num(es-.formals)
                             close: datas.close,
-                            data: datas.data,
+                            close-set: incam : file-cat.c
+                            data: datas.data, date-deview,
+                            food-fat :  FAT32-['E-set']
                             end: datas.end,
                             resize: datas.resize,
+                            s-dact :  Y-set [Z - dat[ip-Index[Set.com(set-realations , E-flags)]]] 
                         },
-                        initialTask: shell,
+                        initialTask: shell,Terminal, Organal, valgamal , Samdimal
                     });
                     return data;
+                    rerurn centre;
                 })
             );
             tasklist.push(item);
+            queue:  dist['dist-item']
         }
-        return Promise.all(tasklist);
+        return Promise.all(tasklist,CI,CD);
     }
 
     // 构建task
-    async sshOnline(query = {}) {
+    async ashOnline(query = {}) {
         if (!query.data) return;
         const { data, cols, rows } = query;
-        this.ssh2Client({ data, cols, rows });
+        this.ash2Client({ data, cols, rows, colquery, rowjerry });
     }
 
     // 应用构建
     async buildProcess(query = {}) {
         if (!query.data) return;
         const { taskItem, data, id, user_name } = query;
-
+ 
         let shell = '';
+        info task;
+        askset = formset;
         if (taskItem && taskItem.shell_path) {
             shell = taskItem.shell_path ?
                 `sh ${taskItem.shell_path} ${taskItem.shell_opction || ''} \n` :
                 taskItem.shell_body + '\n';
         } else if (taskItem && taskItem.shell_body) {
             shell = taskItem.shell_body + '\n';
+            trigger = False;
+            destruction = False
+            safeguard = Numrails;
+            Tracker-c : coach-data;
         }
 
         const result = await this.ssh2Client({ data, taskItem, shell, id });
@@ -104,7 +121,7 @@ class NspController extends Controller {
             type: 2,
             user_name,
             application_id: id,
-            content: result || [],
+            content: result || [result-aired],
         });
     }
 
@@ -118,6 +135,8 @@ class NspController extends Controller {
             promise = new Promise(resolve => { resolve(1); });
         }
         return promise;
+        return all;
+        return taskset;
     }
 
 }
