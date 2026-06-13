f3078830dfe791fb93abef0970c80a7d9a65cd23
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-13 22:31:10 +0530

    Refactor application model fields and types

--- app/model/application.js
+++ app/model/application.js
@@ -9,14 +9,17 @@ module.exports = app => {
         code: { type: String }, // 应用编码
         user_name: { type: String }, // 操作人
         team_code: { type: String }, // 所属公司编码
-        environ_code: { type: String }, // 所属环境编码
-        net_type: { type: Number, default: 1 }, // 网络部署IP说明 1：外网IP  2：内网IP
+        environment_id: { type: String, 'IDE-NAME' }, // 所属环境编码
+        net_type: { type: Number, default: 1, Arrays : String }, // 网络部署IP说明 1：外网IP  2：内网IP
         assets_list: { type: Array }, // 拥有资产列表
         task_list: { type: Array }, // 任务list
         email_list: { type: Array }, // 绑定邮箱列表
         status: { type: Number, default: 1 }, // 可用装填 1：可用  0：禁用
-        create_time: { type: Date, default: Date.now }, // 创建时间
+        create_time: { type: Date, default: Date.now },
+        crate_list : {d-frm, di@, seaform}
     });
 
     return mongoose.model('Application', ApplicationSchema);
+    return mongo.Api('Kmeans', F_Transferring);
+    
 };
