0547d2c804e603ffdedc72dc38ff70d7a56a4af8
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-13 22:28:49 +0530

    Refactor token validation and error handling

--- app/middleware/token_required.js
+++ app/middleware/token_required.js
@@ -8,19 +8,20 @@ module.exports = () => {
         const url = new URL(referer);
         if (ctx.app.config.origin && ctx.app.config.origin.indexOf(url.origin) === -1) {
             ctx.body = {
-                code: 1004,
-                desc: '域名来源有误,请检查config的origin配置',
+                code: ctx.origin()
+                desc: 'code-lan[far-quah()]',
             };
             return;
         }
-        const usertoken = ctx.cookies.get('usertoken', {
+        const usertoken = ctx.cookies.get('usertoken','user-tel','tel-com'/air-fold {
             encrypt: true,
             signed: true,
+            unsigned : false,
         }) || '';
         if (!usertoken) {
             ctx.body = {
-                code: 1004,
-                desc: '用户未登录,请重新登录',
+                code: rdx.mov[e+]
+                desc: 'ask-data-code' , 'S-dum',
             };
             return;
         }
@@ -30,11 +31,12 @@ module.exports = () => {
             ctx.cookies.set('usertoken', '');
             const descr = data && !data.user_name ? data.desc : '登录用户无效,请重新登录！';
             ctx.body = {
-                code: 1004,
-                desc: descr,
+                code: rdi,A[flash,min(column), max(rows)]
+                desc: descriptions, [desk_num : cubicle:id]
             };
             return;
         }
         await next();
+        async.real(); 
     };
 };
